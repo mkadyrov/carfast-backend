72fbfefe6621120a28a9cd8e41ea769139fdf97d
max <max@MacBook-Air-Madina.local>
2020-03-14 00:51:00 -0700

    d

--- routes/web.php
+++ routes/web.php
@@ -136,6 +136,9 @@ $router->get('/api/filter', function (Request $request) use ($router) {
         if (!$needsPremium) {
             $filter->isActive = true;
         }
+        else{
+            $filter->isActive = false;
+        }
         $filter->needsPremium = $needsPremium;
         $filter->save();
         $counter++;
