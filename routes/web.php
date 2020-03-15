c6a80506cba3ceb1b2000874080cb43176c97e91
max <max@MacBook-Air-Madina.local>
2020-03-15 14:01:28 -0700

    ready

--- routes/web.php
+++ routes/web.php
@@ -132,7 +132,7 @@ $router->get('/api/filter', function (Request $request) use ($router) {
             if ($filter->region === '' || $filter->brand === '' || $filter->model === '') {
                 $needsPremium = true;
             }
-            if ($filter->gearbox !== "") {
+            if ($filter->gearbox == "Механика" || $filter->gearbox == "Автомат" ) {
                 $needsPremium = true;
             }
             if (strlen($filter->condition) > 0 || $filter->isCleared === true || $filter->isCleared === false) {
