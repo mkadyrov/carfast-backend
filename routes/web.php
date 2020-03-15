5fd31da3ff5145deb0eb059453b6876fd0cc7dc8
max <max@MacBook-Air-Madina.local>
2020-03-14 22:29:18 -0700

    f

--- routes/web.php
+++ routes/web.php
@@ -74,7 +74,7 @@ $router->post('/api/filter/save', function (Request $request) use ($router) {
         if (!empty($filter->_id)) {
             $client = new GuzzleHttp\Client();
             $user = User::where("chat_id",$json['user'])->first();
-            $res = $client->post('http://167.99.218.57:3003/api/filter/new/' .$user->_id, [
+            $res = $client->post('http://167.99.218.57:3003/api/filter/new/'.$json['user'], [
                 'json' => $filter
             ]);
         }
